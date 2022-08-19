0ec597f25839587788b9c4930bf5525f4c272cae
Suyog Pandit <sygpandit031@gmail.com>
2022-08-19 14:43:44 +0545

    UPDATE file

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
