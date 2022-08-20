b891d455ede3dbd74df433e31fd6e3d4f3389339
Suyog Pandit <sygpandit031@gmail.com>
2022-08-20 21:22:14 +0545

    Add-File

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TypeConversion
+{
+    class TypeConversion
+    {
+        static void Main(String[] args)
+        {
+            int a = 1;
+            double b = 23523.234;
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(Convert.ToString(b));
+        }
+    }
+}
