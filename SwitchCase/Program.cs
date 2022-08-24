5805300e244829b841d398ed08f3519a4276412c
Suyog Pandit <sygpandit031@gmail.com>
2022-08-24 21:03:38 +0545

    Update File

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SwitchCase
+{
+    class SwitchCase
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string a = Console.ReadLine();
+            int s = Convert.ToInt32(a);
+            // using switch case to display days with number inputs 
+            switch (s)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+        }
+    }
+}
