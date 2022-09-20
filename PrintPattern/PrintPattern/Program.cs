f4e5874b2e9dba285c941d6c0a90b86fee74a5fc
Suyog Pandit <71134352+sygpandit@users.noreply.github.com>
2022-09-20 15:28:32 +0545

    Update

---
+++ PrintPattern/PrintPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rem, rev = 0;
+            int num = Convert.ToInt32(Console.ReadLine());
+            int temp = num;
+            while (num > 0)
+            {
+                rem = num % 10;
+                rev = rev * 10 + rem;
+                num = num / 10;
+            }
+            if (temp == rev)
+            {
+                Console.WriteLine("Palindrome");
+
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+        }
+    }
+}
