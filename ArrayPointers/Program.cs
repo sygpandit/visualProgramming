b23ccde44642222c5d2440dbedffa4418caf9722
sygpandit <sygpandit@gmail.com>
2023-02-05 22:55:33 +0545

    Update

---
+++ ArrayPointers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ArrayPointers
+{
+    class ArrayPointers
+    {
+        static unsafe void Main(string[] args)
+        {
+            int[] num = { 1, 2 };
+            fixed (int* p = num)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine((int)(p + i));
+                }
+            }
+
+        }
+    }
+}
